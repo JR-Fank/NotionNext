651e7285ef4d693a6beaf36b59e10141e1c69e64
JR-Fank <62536004+JR-Fank@users.noreply.github.com>
2023-08-15 16:10:18 +0800

    Update AnalyticsCard.js

--- themes/heo/components/AnalyticsCard.js
+++ themes/heo/components/AnalyticsCard.js
@@ -10,7 +10,7 @@ import CONFIG from '../config'
   const today = new Date()
   const diffTime = today.getTime() - targetDate.getTime() // 获取两个日期之间的毫秒数差值
   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) // 将毫秒数差值转换为天数差值
-
+*/
   const { postCount } = props
   return <>
         <div className='text-md flex flex-col space-y-1 justify-center px-3'>
