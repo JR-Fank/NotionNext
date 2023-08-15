29d80e3e28e273a82398d9161378e356701228c4
JR-Fank <62536004+JR-Fank@users.noreply.github.com>
2023-08-15 20:03:04 +0800

    Update index.js

--- themes/heo/index.js
+++ themes/heo/index.js
@@ -186,17 +186,7 @@ const LayoutArchive = (props) => {
   return <LayoutBase {...props} slotRight={slotRight} headerSlot={headerSlot}>
         <div className='p-5 rounded-xl border dark:border-gray-600 max-w-6xl w-full bg-white dark:bg-[#1e1e1e]'>
             {/* 文章分类条 */}
-            <CategoryBar {...props} border={false} />
-
-            <div className='px-3'>
-                {Object.keys(archivePosts).map(archiveTitle => (
-                    <BlogPostArchive
-                        key={archiveTitle}
-                        posts={archivePosts[archiveTitle]}
-                        archiveTitle={archiveTitle}
-                    />
-                ))}
-            </div>
+            
         </div>
     </LayoutBase>
 }
