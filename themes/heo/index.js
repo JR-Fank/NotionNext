899c2801a660594a10e3521219d58b115d039cc6
JR-Fank <62536004+JR-Fank@users.noreply.github.com>
2023-08-15 20:27:14 +0800

    Update index.js

--- themes/heo/index.js
+++ themes/heo/index.js
@@ -90,7 +90,10 @@ const LayoutIndex = (props) => {
     </header>
 
   // 右侧栏 用户信息+标签列表
+  
 
+  return <LayoutBase {...props} slotRight={slotRight} headerSlot={headerSlot}>
+        <div id='post-outer-wrapper' className='px-5 md:px-0'>
             {/* 文章分类条 */}
             <CategoryBar {...props} />
             {BLOG.POST_LIST_STYLE === 'page' ? <BlogPostListPage {...props} /> : <BlogPostListScroll {...props} />}
