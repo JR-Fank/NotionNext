151c41be6cca418a91fe6ed7a489e293c538f7c3
JR-Fank <62536004+JR-Fank@users.noreply.github.com>
2023-08-16 03:23:11 +0800

    Update ArticleAdjacent.js

--- themes/heo/components/ArticleAdjacent.js
+++ themes/heo/components/ArticleAdjacent.js
@@ -3,8 +3,20 @@ import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import CONFIG from '../config'
 
+/**
+ * 上一篇，下一篇文章
+ * @param {prev,next} param0
+ * @returns
+ */
+export default function ArticleAdjacent({ prev, next }) {
+  const [isScrollEnd, setIsScrollEnd] = useState(false)
+  const router = useRouter()
 
+  useEffect(() => {
+    setIsScrollEnd(false)
+  }, [router])
 
+  useEffect(() => {
     // 文章是否已经到了底部li
     const targetElement = document.getElementById('article-end')
 
@@ -71,4 +83,4 @@ import CONFIG from '../config'
 
         </div>
   )
-
+}
