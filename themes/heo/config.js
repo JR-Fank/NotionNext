698f1e0eea12ad867eaac0cbfb16aaa1f8210859
JR-Fank <62536004+JR-Fank@users.noreply.github.com>
2023-08-15 17:35:38 +0800

    Add files via upload

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,10 +1,13 @@
 const CONFIG = {
   HOME_BANNER_ENABLE: true,
 
-  SITE_CREATE_TIME: '2019', // 建站日期，用于计算网站运行的第几天
+  SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
-  
+  NOTICE_BAR: [
+    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
+    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+  ],
 
   // 英雄区(首页顶部大卡)
   HERO_TITLE_1: '分享编程',
@@ -103,29 +106,29 @@ const CONFIG = {
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
-  MENU_INDEX: false, // 显示首页
+  MENU_INDEX: true, // 显示首页
   MENU_CATEGORY: true, // 显示分类
   MENU_TAG: true, // 显示标签
-  MENU_ARCHIVE: false, // 显示归档
-  MENU_SEARCH: false, // 显示搜索
+  MENU_ARCHIVE: true, // 显示归档
+  MENU_SEARCH: true, // 显示搜索
 
   POST_LIST_COVER: true, // 列表显示文章封面
   POST_LIST_COVER_HOVER_ENLARGE: false, // 列表鼠标悬停放大
 
   POST_LIST_COVER_DEFAULT: true, // 封面为空时用站点背景做默认封面
   POST_LIST_SUMMARY: true, // 文章摘要
-  POST_LIST_PREVIEW: true, // 读取文章预览
+  POST_LIST_PREVIEW: false, // 读取文章预览
   POST_LIST_IMG_CROSSOVER: true, // 博客列表图片左右交错
 
-  ARTICLE_ADJACENT: false, // 显示上一篇下一篇文章推荐
+  ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
   ARTICLE_COPYRIGHT: true, // 显示文章版权声明
-  ARTICLE_RECOMMEND: false, // 文章关联推荐
+  ARTICLE_RECOMMEND: true, // 文章关联推荐
 
   WIDGET_LATEST_POSTS: true, // 显示最新文章卡
   WIDGET_ANALYTICS: false, // 显示统计卡
   WIDGET_TO_TOP: true,
-  WIDGET_TO_COMMENT: false, // 跳到评论区
-  WIDGET_DARK_MODE: false, // 夜间模式
-  WIDGET_TOC: false // 移动端悬浮目录
+  WIDGET_TO_COMMENT: true, // 跳到评论区
+  WIDGET_DARK_MODE: true, // 夜间模式
+  WIDGET_TOC: true // 移动端悬浮目录
 }
 export default CONFIG
