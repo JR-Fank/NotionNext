327c265388cc18855b402ad02551ec1e8c2bc28d
JR-Fank <62536004+JR-Fank@users.noreply.github.com>
2023-08-16 16:41:30 +0800

    Update ArticleAdjacent.js

--- themes/heo/components/ArticleAdjacent.js
+++ themes/heo/components/ArticleAdjacent.js
@@ -18,7 +18,29 @@ export default function ArticleAdjacent({ prev, next }) {
   useEffect(() => {
 
     // 文章是否已经到了底部
+    const targetElement = document.getElementById('article-end')
 
+    const handleIntersect = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsScrollEnd(true)
+        }
+      })
+    }
+
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
+    }
+
+    const observer = new IntersectionObserver(handleIntersect, options)
+    observer.observe(targetElement)
+
+    return () => {
+      observer.disconnect()
+    }
+  }, [])
 
   if (!prev || !next || !CONFIG.ARTICLE_ADJACENT) {
     return <></>
@@ -47,17 +69,6 @@ export default function ArticleAdjacent({ prev, next }) {
             </section>
 
             {/* 桌面端 */}
-
-            <div id='pc-next-post' className={`hidden md:block fixed z-40 right-24 bottom-4 duration-200 transition-all ${isScrollEnd ? 'mb-0 opacity-100' : '-mb-24 opacity-0'}`}>
-                <Link
-                    href={`/${next.slug}`}
-                    className='cursor-pointer drop-shadow-xl duration transition-all h-24 dark:bg-[#1e1e1e] border dark:border-gray-600 p-3 bg-white dark:text-gray-300 dark:hover:text-yellow-600 hover:text-white hover:font-bold hover:bg-blue-600 rounded-lg flex flex-col justify-between'
-                >
-                    <div className='text-xs'>下一篇</div>
-                    <hr />
-                    <div>{next?.title}</div>
-                </Link>
-            </div>
 
         </div>
   )
