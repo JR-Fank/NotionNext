aabb836ad15dfeccca7261432415bfeb93f197c7
JR-Fank <62536004+JR-Fank@users.noreply.github.com>
2023-08-16 17:07:34 +0800

    Update ArticleCopyright.js

--- themes/heo/components/ArticleCopyright.js
+++ themes/heo/components/ArticleCopyright.js
@@ -18,11 +18,5 @@ export default function ArticleCopyright () {
     return <></>
   }
 
-  return (
-    <section className="dark:text-gray-300 mt-6 mx-1 ">
-      <ul className="overflow-x-auto whitespace-nowrap text-sm dark:bg-gray-900 bg-gray-100 p-5 leading-8 border-l-2 border-indigo-500">
 
-      </ul>
-    </section>
-  )
 }
